f52c889f0f52c0b16ab1ca330660243273a09f89
PavanKumarrG <45650590+PavanKumarrG@users.noreply.github.com>
2019-10-08 13:35:40 +0530

    commitimng

--- RestFullWebService_1/src/main/java/org/user/app/jpa/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.user.app.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserRepository extends JpaRepository<org.user.app.jpa.User, Integer>{
-
-}
--- RestFullWebService_1/target/classes/user.sql
+++
@@ -1,2 +0,0 @@
-insert into user(id, name, dob) values(2, 'Janl' , sysdate());
-insert into user(id, name, dob) values(3, 'Jing' , sysdate());
